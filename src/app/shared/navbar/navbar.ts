517010445b27c190e662aee9097548a4e24a690e
Daniel Sandberg <DSandDev93@gmail.com>
2025-07-12 21:29:19 +0200

    Add navbar

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { NavLink } from './nav-link';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'pl-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+  @Input({ required: false }) links: NavLink[] = [
+    { label: 'Home', route: '/' }
+    // Add more default links here if needed
+  ];
+}
